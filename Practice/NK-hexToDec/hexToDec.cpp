d3ac1026db9c7c3c394af948c819cca9fad73b3f
pench3r <nageshangzei@gmail.com>
2018-11-06 18:05:18 +0800

    add hexToDec

---
+++ Practice/NK-hexToDec/hexToDec.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int input;
+	while (cin >> hex >> input) {
+		cout << input << endl;
+	}
+	return 0;
+}
